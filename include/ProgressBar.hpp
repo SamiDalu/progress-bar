5d786f319a4c21ae6c5ac274bdf87740d7929121
SamiDalu <115953559+SamiDalu@users.noreply.github.com>
2023-01-11 23:49:35 -0600

    Fixed initialization constructor signature

--- include/ProgressBar.hpp
+++ include/ProgressBar.hpp
@@ -1,12 +1,12 @@
 class ProgressBar {
   private:
     string name_;
-    char[] outer_chars_;
+    char[2] outer_chars_;
     char inner_char_;
     unsigned int length_;
     double progress_ = 0.0;
   public:
-    ProgressBar(string name, char[] outer_chars,  char inner_char, int length);
+    ProgressBar(string name, char[2] outer_chars,  char inner_char, int length);
     ProgressBar();
     void updateProgress(double increase);
     void display();
