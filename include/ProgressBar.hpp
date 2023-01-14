b380541836493652d56cb8202abd529abb7689ca
sanaa dalu <asbestossine5@gmail.com>
2023-01-14 13:25:37 -0600

    fixed char array errors

--- include/ProgressBar.hpp
+++ include/ProgressBar.hpp
@@ -1,13 +1,14 @@
 #include <string>
+
 class ProgressBar {
   private:
     std::string name_;
-    char outer_chars_ [2];
+    std::string outer_chars_;
     char inner_char_;
     unsigned int length_;
     double progress_ = 0.0;
   public:
-    ProgressBar(std::string name, char outer_chars [2],  char inner_char, int length);
+    ProgressBar(std::string name, std::string outer_chars,  char inner_char, int length);
     ProgressBar();
     void updateProgress(double increase);
     void display();
--- src/ProgressBar.cpp
+++ src/ProgressBar.cpp
@@ -1,10 +1,9 @@
 #include <iostream>
-#include "ProgressBar.hpp"
+#include "../include/ProgressBar.hpp"
 
-ProgressBar::ProgressBar(string name, char[] outer_chars, char inner_char, int length) : name_(name), outer_chars_(outer_chars),
-inner_char_(inner_char), length_(length); //initialization constructor 
+ProgressBar::ProgressBar(std::string name, std::string outer_chars, char inner_char, int length) : name_(name), outer_chars_(outer_chars), inner_char_(inner_char), length_(length) {} //initialization constructor 
 
-ProgressBar::ProgressBar() : name_("Progress"), outer_chars_( {'[', ']'} ), inner_char_('█'), length_(50); //default constructor
+ProgressBar::ProgressBar() : name_("Progress"), outer_chars_( "[]" ), inner_char_('|'), length_(50) {} //default constructor
 
 void ProgressBar::updateProgress(double increase) {
   progress_ += increase;
@@ -13,13 +12,13 @@ void ProgressBar::updateProgress(double increase) {
 void ProgressBar::display() {
   std::cout<<name_<<std::endl;
   std::cout<<outer_chars_[0];
-  for (int i = 0; i < length_); i++) {
+  for (int i = 0; i < length_; i++) {
       if (i<= (int) (progress_ * length_)) 
         std::cout<<inner_char_; //prints bar's inner character (representing progress) if i <= progress end
       else
          std::cout<<" "; //prints space otherwise
   }
-  std::cout<<outer_chars[1]<<" "<<(int) (progress*100)<<"%";
+  std::cout<<outer_chars_[1]<<" "<<(int) (progress_*100)<<"%";
 }
 
 void ProgressBar::reset() {
