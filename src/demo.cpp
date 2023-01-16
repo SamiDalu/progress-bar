579000e200c8a0653dfd62827d420c7e051cb21d
sanaa dalu <asbestossine5@gmail.com>
2023-01-16 00:15:54 -0600

    added demonstration of default bar

--- src/demo.cpp
+++ src/demo.cpp
@@ -3,11 +3,17 @@
 
 int main() {
 	ProgressBar bar = ProgressBar("Download Progress", "[]", '#', 20);
+	ProgressBar dflt = ProgressBar(); //default bar
 	while (bar.getProgress() < 1.0) {
 		//given program can decide specifically when to update the bar
-		bar.updateProgress(0.18); //for demonstrative purposes
+		bar.updateProgress(0.35); //for demonstrative purposes
 		bar.display();
 	}
-	std::cout<<"\nDownload complete!";
+	std::cout<<"\nDownload complete!"<<std::endl;
+	
+	while (dflt.getProgress() < 1.0) {
+		dflt.updateProgress(0.35); //for demonstrative purposes
+		dflt.display();
+	}
 	return 0;
 }
